407f70537141f5e5e17eb9db4ee27771963011cc
RadaevaVera <pobratimlok@gmail.com>
2022-12-29 01:59:20 +0300

    fix 4

--- 4.hs
+++ 4.hs
@@ -1,7 +1,13 @@
-
 largestPalindromeProduct :: Int
-largestPalindromeProduct = head $
-  [ x * y | x <- [999,998..100], y <- [999,998..100], isPalindrom (x * y) ]
+largestPalindromeProduct = foldl max 0 $ map mulOnList [999,998..100]
+
+mulOnList :: Int -> Int
+mulOnList x = 
+    let res = filter isPalindrom  $ map (\y -> x * y) [999,998..x]
+    in
+        if (null res)
+        then 0
+        else head res
 
 isPalindrom :: Int -> Bool
 isPalindrom n = n == reverseNumber n 0
@@ -11,4 +17,4 @@ reverseNumber 0 res = res
 reverseNumber n res = reverseNumber (n `div` 10) (res * 10 + n `mod` 10)
 
 -- *Main> largestPalindromeProduct
--- 580085
+-- 906609
